8c3bf4c85a6cc64b9fe3a93e0c231fe0cc4adeb7
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2023-01-25 12:22:40 +0530

    #include<stdio.h> int main() {     int n,i,s;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         s=2*i;         printf("%d ",s);     } }

--- First_N_even_Natural_Numbers.c
+++ First_N_even_Natural_Numbers.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,s;
+    int n,i,s;
     scanf("%d",&n);
     for(i=n;i>=1;i--)
     {
-        s=i*2;
-    printf("%d ",s);
+        s=2*i;
+        printf("%d ",s);
     }
 }
