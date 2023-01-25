e1439862a24fac8d7ea410981a35a32901fa5baa
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2023-01-25 12:08:18 +0530

    #include<stdio.h> int main() {     int i,n,s;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         s=3*i;         printf("%d ",s);     } }

--- first_N_multiples_of_3.c
+++ first_N_multiples_of_3.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,m;
+    int i,n,s;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        m=3*i;
-        printf("%d ",m);
+        s=3*i;
+        printf("%d ",s);
     }
 }
