6a4df229e9e1cc42abe8c05060dbfa2393c7ae6b
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2023-10-18 16:09:20 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     int a[n][n],b[n][n];     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             scanf("%d",&b[i][j]);         }     }     int c[n][n];     for(i=-0;i<n;i++)     {         for(j=0;j<n;j++)         {             c[i][j]=a[i][j]+b[i][j];             printf("%d ",c[i][j]);         }         printf(" ");     } }

---
+++ Addition_of_two_matrices.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    int a[n][n],b[n][n];
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    int c[n][n];
+    for(i=-0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            c[i][j]=a[i][j]+b[i][j];
+            printf("%d ",c[i][j]);
+        }
+        printf("
+");
+    }
+}
